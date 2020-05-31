46b3e1764cc84956df3f004f602a8046d164adfc
Juliana Caro <julianacarom2@gmail.com>
2020-05-30 21:49:37 -0500

    comentarios

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -6,17 +6,17 @@ let lienzo =d.getContext("2d");
 
 // para empezar a dibujar = lapiz sobre "papel", para invocar funcione necesitan paratensis de apertura y cierre.
 lienzo.beginPath();
-// atributo o propiedad del objeto lienzo
+// atributo o propiedad del objeto
 lienzo.strokeStyle = "red";
 // mover al punto 100, 100 del canvas
 lienzo.moveTo(100, 0);
 // linea diaganol hasta el punto 200, 200
 lienzo.lineTo(150, 200);
-// cerramos la linea
 lienzo.moveTo(100, 0);
 lienzo.lineTo(50, 200);
 lienzo.moveTo(75, 100);
 lienzo.lineTo(126, 100);
+// cerrar linea
 lienzo.stroke();
 // terminamos de dibujar = levantar lapiz
 lienzo.closePath();
