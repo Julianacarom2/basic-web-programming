b72610046f69d6a3bbaa4e40a4fd16759fcd5ab0
Juliana Caro <julianacarom2@gmail.com>
2020-06-10 13:46:50 -0500

    dibujo con telcado

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -7,24 +7,47 @@ var teclas =
 };
 console.log (teclas);
 
-document.addEventListener("keyup", dibujarTeclado);
+document.addEventListener("keydown", dibujarTeclado);
+var cuadrito = document.getElementById("area_de_dibujo");
+var papel = cuadrito.getContext("2d");
+var x = 150;
+var y = 150;
+dibujarLinea ("red", 149,149,151,151,papel);
+
+
+function dibujarLinea (color, x_ini, y_ini, x_fin, y_fin,lienzo)
+{
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.lineWidth = 3; 
+    lienzo.moveTo(x_ini, y_ini);
+    lienzo.lineTo(x_fin, y_fin);
+    lienzo.stroke();
+    lienzo.closePath();
+
+}
 
 function dibujarTeclado(evento)
 {
-
+    var colorcito = "blue";
+    var movimiento = 10;
     switch (evento.keyCode)
     {
         case teclas.UP:
-            console.log ("arriba");
+           dibujarLinea (colorcito, x, y, x, y-movimiento, papel)
+           y = y - movimiento;
         break;
         case teclas.DOWN:
-            console.log ("abajo");
+            dibujarLinea (colorcito, x, y, x, y+movimiento, papel )
+            y = y + movimiento;
         break;
         case teclas.LEFT:
-            console.log ("izquierda")
+            dibujarLinea (colorcito, x, y, x- movimiento, y, papel )
+            x = x - movimiento;
         break;
         case teclas.RIGHT:
-            console.log ("derecha")
+            dibujarLinea (colorcito, x, y, x+movimiento, y, papel )
+            x = x + movimiento;
         break;
         default:
             console.log("otra tecla");
